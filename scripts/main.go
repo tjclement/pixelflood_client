b173bb829c4986ebddefaa75e7cade0a2e8a4b85
Tom <tom.clement@bizboard.nl>
2017-08-17 02:09:18 +0200

    Fixed param flags being overwritten by defaults

--- scripts/main.go
+++ scripts/main.go
@@ -16,10 +16,10 @@ import (
 
 func main() {
 	image_file := flag.String("image", "./image.jpg", "X value of the screen to begin drawing on")
-	x_start := *flag.Int("x_start", 0, "X value of the screen to begin drawing on")
-	y_start := *flag.Int("y_start", 0, "y value of the screen to begin drawing on")
-	x_size := *flag.Int("x_size", 320, "X size of the image we want to draw on the screen")
-	y_size := *flag.Int("y_size", 320, "Y size of the image we want to draw on the screen")
+	x_start := flag.Int("x_start", 0, "X value of the screen to begin drawing on")
+	y_start := flag.Int("y_start", 0, "y value of the screen to begin drawing on")
+	x_size := flag.Int("x_size", 320, "X size of the image we want to draw on the screen")
+	y_size := flag.Int("y_size", 320, "Y size of the image we want to draw on the screen")
 	worker_amount := flag.Int("worker_amount", 16, "The amount of workers that concurrently sends pixel values to the server")
 	worker_type := flag.String("worker_type", "squares", "The type of pixel replacement strategy ['random', 'squares']")
 	screen := flag.String("screen", "", "The screen to target ['topleft', 'center', 'topright', 'externalright']. Replaces manual x,y start and size.")
@@ -28,13 +28,13 @@ func main() {
 	if len(*screen) > 0 {
 		switch *screen {
 		case "topleft":
-			x_start, y_start, x_size, y_size = 0, 0, 80, 80
+			*x_start, *y_start, *x_size, *y_size = 0, 0, 80, 80
 		case "center":
-			x_start, y_start, x_size, y_size = 80, 24, 240, 240
+			*x_start, *y_start, *x_size, *y_size = 80, 24, 240, 240
 		case "topright":
-			x_start, y_start, x_size, y_size = 320, 0, 80, 80
+			*x_start, *y_start, *x_size, *y_size = 320, 0, 80, 80
 		case "externalright":
-			x_start, y_start, x_size, y_size = 400, 0, 80, 80
+			*x_start, *y_start, *x_size, *y_size = 400, 0, 80, 80
 		}
 	}
 
@@ -45,7 +45,7 @@ func main() {
 		return
 	}
 
-	image, _, err := image.Decode(reader)
+	imageData, _, err := image.Decode(reader)
 
 	if err != nil {
 		fmt.Printf("Error parsing image file: %s\r\n", err.Error())
@@ -53,10 +53,10 @@ func main() {
 	}
 
 	// Resize the image to fit our screen's dimensions
-	image = resize.Resize(uint(x_size), uint(y_size), image, resize.NearestNeighbor)
+	imageData = resize.Resize(uint(*x_size), uint(*y_size), imageData, resize.NearestNeighbor)
 
-	sender := pixelflood_client.CreateSender(x_start, y_start, *worker_amount)
-	sender.SetImage(image)
+	sender := pixelflood_client.CreateSender(*x_start, *y_start, *worker_amount)
+	sender.SetImage(imageData)
 	sender.Start(*worker_type)
 
 	bufio.NewScanner(os.Stdin).Scan()
